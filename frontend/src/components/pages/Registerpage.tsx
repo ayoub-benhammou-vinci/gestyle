f6b2dc830b59e28c0ebb45cb9d73daa98459fb85
Ayoub <ayoub.benhammou@student.vinci.be>
2025-09-11 20:02:21 +0200

    refactor: add validators when a person want to register

--- frontend/src/components/pages/Registerpage.tsx
+++ frontend/src/components/pages/Registerpage.tsx
@@ -87,123 +87,135 @@ const RegisterPage = () => {
     }
   };
   return (
-    <Box sx={{ flex: 1, py: 5, textAlign: 'center' }}>
-      <Box component="form" sx={{ textAlign: 'left' }} onSubmit={handleSubmit}>
-        <Typography
-          variant="h4"
-          fontWeight="bold"
-          sx={{ color: '#9c684e', mb: 4 }}
-        >
-          Inscription
-        </Typography>
+    <Box
+      component="form"
+      sx={{ flex: 1, py: 5, textAlign: 'center' }}
+      onSubmit={handleSubmit}
+    >
+      <Typography
+        variant="h4"
+        fontWeight="bold"
+        sx={{ color: '#9c684e', mb: 4 }}
+      >
+        Inscription
+      </Typography>
 
-        <Typography
-          variant="h4"
-          fontWeight="bold"
-          sx={{ color: '#9c684e', mb: 4 }}
-        >
-          {error && <span style={{ color: 'red' }}>{error}</span>}
-        </Typography>
+      <Typography
+        variant="h4"
+        fontWeight="bold"
+        sx={{ color: '#9c684e', mb: 4 }}
+      >
+        {error && <span style={{ color: 'red' }}>{error}</span>}
+      </Typography>
 
-        <Paper
-          elevation={3}
-          sx={{
-            maxWidth: 900,
-            margin: '0 auto',
-            padding: 4,
-            borderRadius: 5,
-            backgroundColor: '#f5eee7',
-          }}
-        >
-          <Grid container spacing={3}>
-            {/* Colonne gauche */}
-            <Grid item xs={12} md={6}>
-              <Box textAlign="left">
-                <Box sx={{ color: '#9c684e', mb: 3.8 }}>
-                  <RowRadioButtonsGroup
-                    option1="Féminin"
-                    option2="Masculin"
-                    option3="Autre"
-                    value={sexe}
-                    onChange={(val) => setSexe(val)}
-                  />
-                </Box>
-
-                <Typography sx={{ color: '#9c684e', mb: 1 }}>Pseudo</Typography>
-                <TextField
-                  fullWidth
-                  variant="outlined"
-                  sx={{ backgroundColor: '#fff' }}
-                  value={pseudo}
-                  onChange={(e) => setPseudo(e.target.value)}
-                  required
+      <Paper
+        elevation={3}
+        sx={{
+          maxWidth: 900,
+          margin: '0 auto',
+          padding: 4,
+          borderRadius: 5,
+          backgroundColor: '#f5eee7',
+        }}
+      >
+        <Grid container spacing={3}>
+          {/* Colonne gauche */}
+          <Grid item xs={12} md={6}>
+            <Box textAlign="left">
+              <Box sx={{ color: '#9c684e', mb: 3.8 }}>
+                <RowRadioButtonsGroup
+                  option1="Féminin"
+                  option2="Masculin"
+                  option3="Autre"
+                  value={sexe}
+                  onChange={(val) => setSexe(val)}
                 />
               </Box>
-            </Grid>
 
-            {/* Colonne droite */}
-            <Grid item xs={12} md={6}>
-              <Box textAlign="left">
-                <Typography sx={{ color: '#9c684e', mb: 1 }}>
-                  Adresse e-mail
-                </Typography>
-                <TextField
-                  fullWidth
-                  variant="outlined"
-                  sx={{ mb: 1, backgroundColor: '#fff' }}
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-
-                <Typography sx={{ color: '#9c684e', mb: 1 }}>
-                  Mot de passe
-                </Typography>
-                <TextField
-                  fullWidth
-                  type="password"
-                  variant="outlined"
-                  sx={{ mb: 1, backgroundColor: '#fff' }}
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-
-                <Typography sx={{ color: '#9c684e', mb: 1 }}>
-                  Répéter le mot de passe
-                </Typography>
-                <TextField
-                  fullWidth
-                  type="password"
-                  variant="outlined"
-                  sx={{ backgroundColor: '#fff' }}
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                />
-              </Box>
-            </Grid>
+              <Typography sx={{ color: '#9c684e', mb: 1 }}>Pseudo</Typography>
+              <TextField
+                fullWidth
+                variant="outlined"
+                sx={{ backgroundColor: '#fff' }}
+                value={pseudo}
+                onChange={(e) => setPseudo(e.target.value)}
+                required
+              />
+            </Box>
           </Grid>
 
-          {/* Bouton */}
-          <Box mt={4}>
-            <Button
-              variant="contained"
-              sx={{
-                backgroundColor: '#a87052',
-                color: '#fff',
-                paddingX: 4,
-                paddingY: 1,
-                borderRadius: 3,
-                fontWeight: 'bold',
-                '&:hover': {
-                  backgroundColor: '#915e45',
-                },
-              }}
-              type="submit"
-            >
-              S’inscrire
-            </Button>
-          </Box>
-        </Paper>
-      </Box>
+          {/* Colonne droite */}
+          <Grid item xs={12} md={6}>
+            <Box textAlign="left">
+              <Typography sx={{ color: '#9c684e', mb: 1 }}>
+                Adresse e-mail
+              </Typography>
+              <TextField
+                fullWidth
+                variant="outlined"
+                sx={{ mb: 1, backgroundColor: '#fff' }}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                required
+              />
+
+              <Typography sx={{ color: '#9c684e', mb: 1 }}>
+                Mot de passe
+              </Typography>
+              <TextField
+                fullWidth
+                type="password"
+                variant="outlined"
+                sx={{ mb: 1, backgroundColor: '#fff' }}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                inputProps={{
+                  pattern:
+                    '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z0-9]).{8,}$',
+                  title:
+                    'Au moins 8 caractères avec 1 majuscule, 1 minuscule, 1 chiffre et 1 symbole',
+                }}
+                required
+              />
+
+              <Typography sx={{ color: '#9c684e', mb: 1 }}>
+                Répéter le mot de passe
+              </Typography>
+              <TextField
+                fullWidth
+                type="password"
+                variant="outlined"
+                sx={{ backgroundColor: '#fff' }}
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
+              />
+            </Box>
+          </Grid>
+        </Grid>
+
+        {/* Bouton */}
+        <Box mt={4}>
+          <Button
+            variant="contained"
+            sx={{
+              backgroundColor: '#a87052',
+              color: '#fff',
+              paddingX: 4,
+              paddingY: 1,
+              borderRadius: 3,
+              fontWeight: 'bold',
+              '&:hover': {
+                backgroundColor: '#915e45',
+              },
+            }}
+            type="submit"
+          >
+            S’inscrire
+          </Button>
+        </Box>
+      </Paper>
     </Box>
   );
 };
